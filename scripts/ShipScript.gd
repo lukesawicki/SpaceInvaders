7ca44f5db62cb82161c6b568d5c54428364c6c77
luuk-pc <lukaszsa1@gmail.com>
2016-11-27 00:00:01 +0100

    Some changes
    I have no idea what changes that could are?.

--- scripts/ShipScript.gd
+++ scripts/ShipScript.gd
@@ -3,16 +3,22 @@ extends KinematicBody2D
 const SHIP_VELOCITY = 150
 
 var shipVelocityVector = Vector2()
-	
+
 func _fixed_process(delta):
 	
 	shipVelocityVector.y=0
 	
 	if(Input.is_action_pressed("ship_left")):
-		if(not is_colliding()):
+		if(is_colliding()):
+			shipVelocityVector.x=0;
+			shipVelocityVector.y=0;
+		else:
 			shipVelocityVector.x = -SHIP_VELOCITY
 	elif(Input.is_action_pressed("ship_right")):
-		if(not is_colliding()):
+		if(is_colliding()):
+			shipVelocityVector.x=0;
+			shipVelocityVector.y=0;
+		else:
 			shipVelocityVector.x = SHIP_VELOCITY
 	else:
 		shipVelocityVector.x = 0
---
+++ scripts/ShipSkr.gd
@@ -0,0 +1,23 @@
+extends Sprite
+
+const SHIP_VELOCITY = 150
+
+var shipVelocityVector = Vector2()
+
+func _fixed_process(delta):
+	
+	shipVelocityVector.y=0
+	
+	if(Input.is_action_pressed("ship_left")):
+		shipVelocityVector.x = -SHIP_VELOCITY
+	elif(Input.is_action_pressed("ship_right")):
+		shipVelocityVector.x = SHIP_VELOCITY
+	else:
+		shipVelocityVector.x = 0
+	
+	#get_node("Ship1").get_viewport(). = shipVelocityVector * delta
+	get_node("Ship1").get_pos().x = get_node("Ship1").get_global_pos().x + delta * SHIP_VELOCITY
+	
+	
+func _ready():
+	set_fixed_process(true)
