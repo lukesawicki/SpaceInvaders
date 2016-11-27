d083d0fffecbf79340b7ee81c6aacbc2fc931a9f
luuk-pc <lukaszsa1@gmail.com>
2016-11-27 02:00:05 +0100

    Invaders added and script to create
    Invaders added and script to create

--- scripts/Gameplay.gd
+++ scripts/Gameplay.gd
@@ -1,6 +1,29 @@
 extends Node2D
 
+var screen_size
+
+var invader_width = 24
+var invader_hight = 24
+var left_direction = -1
+var right_direction = 1
+var space = 24
+
+var position = 0
 func _ready():
+#	screen_size = get_viewport().
+	var invader_width = 24
+	var invader_hight = 24
+	
+	var nodesInGroupG = get_tree().get_nodes_in_group("InvadersG")
+	
+	for nodeInGroup in nodesInGroupG:
+		position = position + invader_width+space #get_node(nodeInGroup).set_pos(position, 24)
+		nodeInGroup.set_pos(Vector2(position, 24))
+		
+	#for nodeInGroup in groupG:
+		
+		
+
 	set_process(true)
 
 func _process(delta):
