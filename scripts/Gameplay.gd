e25926c9411377f1030581515c1efe9d0827856d
luuk-pc <lukaszsa1@gmail.com>
2016-12-03 00:19:39 +0100

    Ship can shoot and code is more clear
    Ship can shoot and code is more clear

--- scripts/Gameplay.gd
+++ scripts/Gameplay.gd
@@ -1,6 +1,16 @@
 extends Node2D
 
 var laserBeam = preload("res://scenes/LaserBeamShootScene.tscn")
+
+var invaders_g = preload("res://scenes/Ginvaders.tscn")
+var invaders_f = preload("res://scenes/Finvaders.tscn")
+var invaders_e = preload("res://scenes/Einvaders.tscn")
+
+var allInvaders = []
+
+
+#var dominionInstance
+#var nodesInTheDominion
 var laserBeamCount = 0
 var laserBeamArray = []
 #var laserBeamPosition
@@ -23,12 +33,24 @@ var shipLaserMoving = true
 
 var isShootPressed = false
 
-var MARGIN_LEFT = 150
+var MARGIN_LEFT = 20
 var MARGIN_RIGHT = 150
 var MARGIN_TOP = 20
 var MARGIN_BOTTOM = 64
 var SCREEN_WIDTH = 672
 var SCREEN_HIGHT = 768
+
+var INVADERS_G_WIDTH = 24
+var INVADERS_F_WIDTH = 36
+var INVADERS_E_WIDTH = 36
+
+var INVADERS_HIGHT = 8
+
+var INVADERS_ROWS_SPACE = 24
+
+var SPACE_BETWEEN_G = 24
+var SPACE_BETWEEN_F = 15
+var SPACE_BETWEEN_E = 12
 
 var shipVelocityVector = Vector2()
 
@@ -47,49 +69,48 @@ func _ready():
 	var nodesInGroupE = get_tree().get_nodes_in_group("InvadersF")
 	var nodesInGroupF = get_tree().get_nodes_in_group("InvadersE")
 	
-	for nodeInGroup in nodesInGroupG:
-		counter+=1
-		positionX = positionX + invader_width+space #get_node(nodeInGroup).set_pos(position, 24)
-		nodeInGroup.set_pos(Vector2(positionX, positionY))
-	
+	#### tworzenie invadersow
 
-	counter = 0
-	positionX = 0
-	positionY = positionY+48
-	for nodeInGroup in nodesInGroupF:
-		counter+=1
-		positionX = positionX + invader_width+space #get_node(nodeInGroup).set_pos(position, 24)
-		nodeInGroup.set_pos(Vector2(positionX, positionY))
-		if(counter==11):
-			# zmienna y i jego zmiana w if
-			positionY=positionY+48
-			positionX=0
+
+	positionY = MARGIN_TOP
+	positionX = MARGIN_LEFT
+	for i in range(55):
+		if i < 11:
+			var invaderGinstance = invaders_g.instance()
+			invaderGinstance.set_name("InvaderG" + str(i))
+			add_child(invaderGinstance)
+			positionX = positionX + INVADERS_G_WIDTH + SPACE_BETWEEN_G
+			var invaderPosition = Vector2(positionX, positionY)
+			get_node("InvaderG" + str(i)).set_pos(invaderPosition)
+			allInvaders.push_back("InvaderG" + str(i))
+			
+		elif i>10 && i<33:
+			if i == 11 || i == 22:
+				positionX = MARGIN_LEFT
+				positionY = positionY+INVADERS_ROWS_SPACE
+			var invaderFinstance = invaders_f.instance()
+			invaderFinstance.set_name("InvaderF" + str(i))
+			add_child(invaderFinstance)
+			positionX = positionX + INVADERS_F_WIDTH + SPACE_BETWEEN_F #get_node(nodeInGroup).set_pos(position, 24)
+			var invaderPosition = Vector2(positionX, positionY)
+			get_node("InvaderF" + str(i)).set_pos(invaderPosition)
+			allInvaders.push_back("InvaderF" + str(i))
 		
-	counter=0
-	positionX=0
-	positionY = positionY+48
-	for nodeInGroup in nodesInGroupE:
-		counter+=1
-		positionX = positionX + invader_width+space #get_node(nodeInGroup).set_pos(position, 24)
-		nodeInGroup.set_pos(Vector2(positionX, positionY))
-		if(counter==11):
-			# zmienna y i jego zmiana w if
-			positionY=positionY+48
-			positionX=0
-		
-		
-	#for nodeInGroup in groupG:
-		
-		
+		elif i>32 && i<55:
+			if i==33 || i == 44:
+				positionX = MARGIN_LEFT
+				positionY = positionY+INVADERS_ROWS_SPACE
+			var invaderEinstance = invaders_e.instance()
+			invaderEinstance.set_name("InvaderE" + str(i))
+			add_child(invaderEinstance)
+			positionX = positionX + INVADERS_E_WIDTH + SPACE_BETWEEN_E #get_node(nodeInGroup).set_pos(position, 24)
+			var invaderPosition = Vector2(positionX, positionY)
+			get_node("InvaderE" + str(i)).set_pos(invaderPosition)
+			allInvaders.push_back("InvaderE" + str(i))
 
 	set_process(true)
-	#### end _ready() function ####
-	
 
 func _process(delta):
-	#if(Input.is_action_pressed("ship_shoot")):
-		#shipLaser = get_node("ShipLaser").get_pos().x
-		#shipLaser.set_pos(Vector2(330,330))
 	
 	get_node("Ship").movingShip(delta)
 	var ex = get_node("Ship").get_pos().x
@@ -110,6 +131,17 @@ func _process(delta):
 			remove_child(get_node(laser))
 			laserBeamArray.remove(laserId)
 		laserId = laserId + 1
+	
+	var i = 0
+	for OneInvader in allInvaders:
+		if i < 11:
+			get_node("InvaderG" + str(i)).get_pos().x
+		elif i>10 && i<33:
+			pass
+		elif i>32 && i<55:
+			pass
+		i=i+1
+			
 func fire():
 	laserBeamCount = laserBeamCount + 1
 	var laserBeamInstance = laserBeam.instance()
