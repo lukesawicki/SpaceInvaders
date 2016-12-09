7fcc09fbf25a318cc31053cd7f3f5385b93202af
luuk-pc <lukaszsa1@gmail.com>
2016-12-09 16:27:39 +0100

    Collisions work in progress
    Collisions work in progress

--- scripts/Gameplay.gd
+++ scripts/Gameplay.gd
@@ -10,13 +10,9 @@ var invaders_e = preload("res://scenes/E_Invaders.tscn")
 
 var allInvaders = []
 
-
-#var dominionInstance
-#var nodesInTheDominion
 var laserBeamCount = 0
 var laserBeamArray = []
 var temporaryLaserBeam
-#var laserBeamPosition
 
 var screen_size
 
@@ -47,6 +43,9 @@ var INVADERS_G_WIDTH = 24
 var INVADERS_F_WIDTH = 33
 var INVADERS_E_WIDTH = 36
 
+var LASER_BEAM_WIDTH = 4
+var LASER_BEAM_HIGHT = 15
+
 var INVADERS_HIGHT = 24
 
 var INVADERS_ROWS_SPACE = 24
@@ -56,36 +55,17 @@ var SPACE_BETWEEN_F = 15
 var SPACE_BETWEEN_E = 12
 
 var invaderPositionToCheck
+var laserBeamPositionToCheck
 
 var shipVelocityVector = Vector2()
 
-
-
-
 func _ready():
-#	screen_size = get_viewport().
-	#get_node("Ship").set_pos(Vector2(SHIP_X, SHIP_Y));
 
-
-
-	#var invader_width = 24
-	#var invader_hight = 24
-	
-		#if laserBeamCount < 3:
-	#	laserBeamCount = laserBeamCount + 1
-	#	var laserBeamInstance = laserBeam.instance()
-	#	laserBeamInstance.set_name("laser" + str(laserBeamCount))
-	#	#print("fire"+laserBeamInstance.get_name())
-	#	add_child(laserBeamInstance)
-	#	var laserBeamPosition = get_node("laser"+str(laserBeamCount)).get_pos()
-	#	laserBeamPosition.y= get_node("Ship").get_pos().y
-	#	laserBeamPosition.x = get_node("Ship").get_pos().x
-	#	get_node("laser" + str(laserBeamCount)).set_pos(laserBeamPosition)
-	#	laserBeamArray.push_back("laser" + str(laserBeamCount))
 	
 	terranShipp = terranShipScene.instance()
 	terranShipp.set_name("myShip")
 	add_child(terranShipp)
+	
 	
 	for i in range(1):
 		var laserBeamInstance = laserBeam.instance()
@@ -96,13 +76,6 @@ func _ready():
 		laserBeamPosition.x = -32 #500+i*20#get_node("Ship").get_pos().x
 		get_node("LaserBeam" + str(i)).set_pos(laserBeamPosition)
 		laserBeamArray.push_back("LaserBeam" + str(i))
-		print("tworze stateczek numer", i)
-	
-	#### tworzenie invadersow
-	
-#	var nodesInGroupG = get_tree().get_nodes_in_group("InvadersG")
-#	var nodesInGroupE = get_tree().get_nodes_in_group("InvadersF")
-#	var nodesInGroupF = get_tree().get_nodes_in_group("InvadersE")
 	
 
 	positionY = MARGIN_TOP
@@ -144,81 +117,30 @@ func _ready():
 	set_process(true)
 
 func _process(delta):
-	
-#	for i in range(55):
-#		if i < 11:
-#			invaderPositionToCheck = get_node("InvaderG" + str(i)).get_pos()
-#		elif i>10 && i<33:
-#			invaderPositionToCheck = get_node("InvaderF" + str(i)).get_pos()
-#		elif i>32 && i<55:
-#			invaderPositionToCheck = get_node("InvaderE" + str(i)).get_pos()
-		
-	
+	# checking if any bullet is out of level if it is then shoot
 	get_node("myShip").movingShip(delta)
 	if Input.is_action_pressed("ship_shoot"):
 		if !isShootPressed:
 			for oneLaserBeam in laserBeamArray:
 				laserBeamCount = laserBeamCount + 1
-				print("first in for loop")
 				laserBeamPosition = get_node(oneLaserBeam).get_pos()
 				if laserBeamPosition.y < 0:
 					print("out of screen")
 					laserBeamPosition.y = get_node("myShip").get_pos().y
 					laserBeamPosition.x = get_node("myShip").get_pos().x
-					get_node(oneLaserBeam).set_pos(laserBeamPosition)
+					get_node(oneLaserBeam).set_pos(laserBeamPosition) #set laser position to the same position as ship position
 					get_node(oneLaserBeam).moving = true
-					print("ustawione ", laserBeamPosition)
 					break
-			isShootPressed = true	
+			isShootPressed = true
 	else:
 		isShootPressed = false
-	
 	for oneLaserBeam in laserBeamArray:
 		get_node(oneLaserBeam).shooting(delta)
-		for i in range(2,8):
-			pass #get_node("InvaderG" + str(i)).get
-	#var laserId = 0
-	#for laser in laserBeamArray:
-	#	var laserPos = get_node(laser).get_pos()
-	#	laserPos.y = laserPos.y - 150 * delta
-	#	get_node(laser).set_pos(laserPos)
-	#	if laserPos.y < 0:
-	#		remove_child(get_node(laser))
-	#		laserBeamArray.remove(laserId)
-	#		laserBeamCount = laserBeamCount - 1
-	#	laserId = laserId + 1
-	
-	var i = 0
-#	for OneInvader in allInvaders:
-#		if i < 11:
-#			var invaderPosition = get_node("InvaderG" + str(i)).get_pos()
-#			for laserBeamOne in laserBeamArray:
-#				var laserBeamPosition = get_node(laserBeamOne).get_pos()
-#				if laserBeamPosition.x+INVADERS_G_WIDTH/2 > invaderPosition.x-INVADERS_G_WIDTH/2 && laserBeamPosition.x-INVADERS_G_WIDTH/2 > invaderPosition.x-INVADERS_G_WIDTH/2 || laserBeamPosition.y-INVADERS_G_HIGHT/2:
-#					pass
-#		elif i>10 && i<33:  
-#			get_node("InvaderF" + str(i)).get_pos()
-#		elif i>32 && i<55:
-#			get_node("InvaderE" + str(i)).get_pos()
-#		i=i+1
-			
+
 func fire():
 	laserBeamCount = laserBeamCount+1
-	#if laserBeamCount < 3:
-	#	laserBeamCount = laserBeamCount + 1
-	#	var laserBeamInstance = laserBeam.instance()
-	#	laserBeamInstance.set_name("laser" + str(laserBeamCount))
-	#	#print("fire"+laserBeamInstance.get_name())
-	#	add_child(laserBeamInstance)
-	#	var laserBeamPosition = get_node("laser"+str(laserBeamCount)).get_pos()
-	#	laserBeamPosition.y= get_node("Ship").get_pos().y
-	#	laserBeamPosition.x = get_node("Ship").get_pos().x
-	#	get_node("laser" + str(laserBeamCount)).set_pos(laserBeamPosition)
-	#	laserBeamArray.push_back("laser" + str(laserBeamCount))
 	
-	#print(laserBeamArray)
 	
-
 func _on_Main_Menu_pressed():
 	get_node("/root/global").goto_scene("res://scenes/MainMenu.tscn")
 	
--- scripts/ShipShooting.gd
+++ scripts/ShipShooting.gd
@@ -8,5 +8,9 @@ func shooting(delta):
 	if moving:
 		set_pos(Vector2(get_pos().x, get_pos().y + delta * LASER_BEAM_VELOCITY));
 	
+func changeState(isMoving):
+	moving = isMoving
+	
+	
 func _ready():
 	set_process(true)
