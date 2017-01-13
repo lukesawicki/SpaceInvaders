8a32babaf0fb91ea6a98a150b10332052f21e3e7
Lukasz Walczak <luw@concare.net>
2017-01-13 20:52:17 +0100

    add music to EndGame and GameOver scenes

--- scripts/GameOver.gd
+++ scripts/GameOver.gd
@@ -1,5 +1,6 @@
 func _on_BackToMenu_pressed():
 	get_node("/root/global").goto_scene("res://scenes/MainMenu.tscn")
+	get_node("/root/global").menuMusic.play()
 	
 func _on_TryAgain_pressed():
 	get_node("/root/global").goto_scene("res://scenes/Gameplay.tscn")
